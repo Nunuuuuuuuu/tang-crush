f3400cf30d3221a0cd6c16ebdb772730d4c97d44
Nunuuuuuuuu <86741242+Nunuuuuuuuu@users.noreply.github.com>
2021-07-01 19:39:36 +0700

    Create config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     btnNo: 'Đẹp trai siêu cấp vip pro :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu bé <3',
     btnReply: 'Gửi cho bé <3',
-    reply: 'Tại bé cutee phô mai que, xinh đẹp tuyệt trần <33333333',
+    reply: 'Tại bé xinh đẹp tuyệt trần <33333333',
     mess: 'Cảm ơn anh vì đã đến. Yêu anh nhiều nhiều 😘😘',
     messDesc: 'Gặp nhau anh sẽ có quà nha. Còn bây giờ thì thơm em đi nè :>',
     btnAccept: 'Okiiiii lun <3',
