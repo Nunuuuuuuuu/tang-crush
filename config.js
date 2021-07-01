8e503c8bd5aef27dccf7f9ba4ca7e371dc30b639
Nunuuuuuuuu <86741242+Nunuuuuuuuu@users.noreply.github.com>
2021-07-01 17:56:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Anh eooo à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    desc: 'Điều đầu tiên em nghĩ đến anh là gì nè :>>> ',
+    btnYes: 'Anh là con heooo thúiii <3',
+    btnNo: 'Đẹp traii siêu cấp vipp pro :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu bé <3',
+    btnReply: 'Whyyyy <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Cảm ơn vì anh đã đến 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Happy anniversary 9th.',
+    btnAccept: 'Nếu anh thích món quà này thì hãy thơm bé nhiều cái đi nè <3',
+    messLink: 'https://m.me/tuyetnhung191999'
 }
