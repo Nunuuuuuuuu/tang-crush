e0a5d297a1fbad982ee58a6c119463cf8e21f255
Nunuuuuuuuu <86741242+Nunuuuuuuuu@users.noreply.github.com>
2021-07-01 18:46:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'He luuuu Quân iuu!',
+    introDesc: `bé là Tuyết Nhung của Quân nè.
+    Chớp mắt bé lạc vào tim anh cũng được 9 tháng ùi nè`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    desc: 'Điều đầu tiên em nghĩ về anh là gì nè? ',
+    btnYes: 'Anh là một con heo thúi <33',
+    btnNo: 'Đẹp trai siêu cấp vip prooo :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu bé <3',
+    btnReply: 'Gửi cho bé <3',
+    reply: 'Vì em là xinh đẹp tuyệt trần :))))',
+    mess: 'Cảm ơn vì anh đã đến 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Gặp nhau rồi sẽ có quà nha. Còn bây giờ hãy thơm em đeee.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/tuyetnhung191999'
 }
